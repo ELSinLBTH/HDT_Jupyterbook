96dde56a6c6ed63ebaf58c51f9cb9a1ea8f74534
areumhan0423 <158506119+areumhan0423@users.noreply.github.com>
2024-12-09 14:38:04 +0000

    delete js file, create css file

--- _static/reset_links.js
+++
@@ -1,7 +0,0 @@
-// reset_links.js
-window.onload = function () {
-    var links = document.querySelectorAll("a");
-    for (var i = 0; i < links.length; i++) {
-        links[i].style.color = '#0000FF'; // Force all links to be blue
-    }
-};
